1cc0d92c2acba901bde2b88fa0e285d92b4fcbcd
RomanRedkovskiy <romanredkovskiy10@gmail.com>
2023-10-01 23:21:02 +0300

    Add lesson 6 with tests

---
+++ src/test/java/lab1/task6/MatrixFromArrayTest.java
@@ -0,0 +1,48 @@
+package lab1.task6;
+
+import org.example.lab1.task6.MatrixFromArray;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MatrixFromArrayTest {
+
+    private final MatrixFromArray matrixFromArray = new MatrixFromArray();
+
+    @Test
+    void createMatrixWhenEmptyArrayShouldReturnEmptyMatrix() {
+        //given
+        int[] arr = {};
+        int[][] expected = new int[][]{};
+        //when
+        int[][] actual = matrixFromArray.createMatrix(arr);
+        //then
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void createMatrixWhenSingleElementArrayShouldReturnSingleElementMatrix() {
+        //given
+        int[] arr = {1};
+        int[][] expected = new int[][]{new int[]{1}};
+        //when
+        int[][] actual = matrixFromArray.createMatrix(arr);
+        //then
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void createMatrixWhenThreeElementArrayShouldReturnThreeOnThreeMatrix() {
+        //given
+        int[] arr = {10, 23, 31};
+        int[][] expected = new int[][]{
+                new int[]{10, 23, 31},
+                new int[]{23, 31, 10},
+                new int[]{31, 10, 23}
+        };
+        //when
+        int[][] actual = matrixFromArray.createMatrix(arr);
+        //then
+        assertArrayEquals(expected, actual);
+    }
+}
