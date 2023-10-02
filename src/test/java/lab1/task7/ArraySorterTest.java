efb61f0743b3902de346260425fc43484b094af5
RomanRedkovskiy <romanredkovskiy10@gmail.com>
2023-10-02 09:26:34 +0300

    Add lesson 7 with tests

---
+++ src/test/java/lab1/task7/ArraySorterTest.java
@@ -0,0 +1,43 @@
+package lab1.task7;
+
+import org.example.lab1.task7.ArraySorter;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArraySorterTest {
+
+    private final ArraySorter arraySorter = new ArraySorter();
+
+    @Test
+    void sortArrayWhenEmptyArrayShouldReturnEmptyArray() {
+        //given
+        int[] arr = {};
+        int[] expected = {};
+        //when
+        arraySorter.sortArray(arr);
+        //then
+        assertArrayEquals(expected, arr);
+    }
+
+    @Test
+    void sortArrayWhenAlreadySortedShouldReturnInitialArray() {
+        //given
+        int[] arr = {10, 23, 26, 31};
+        //when
+        arraySorter.sortArray(arr);
+        //then
+        assertArrayEquals(arr, arr);
+    }
+
+    @Test
+    void sortArrayWhenUnsortedArrayShouldReturnSortedArray() {
+        //given
+        int[] arr = {23, 31, 10, 26, 5, 4, 1};
+        int[] expected = {1, 4, 5, 10, 23, 26, 31};
+        //when
+        arraySorter.sortArray(arr);
+        //then
+        assertArrayEquals(expected, arr);
+    }
+}
